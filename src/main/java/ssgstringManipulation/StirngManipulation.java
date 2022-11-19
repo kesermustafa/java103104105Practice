ad57f7dc061a39cb39b00a8743161acc20643bad
Mustafa Keser <keserzade@gmail.com>
2022-11-20 00:17:39 +0300

    2

--- src/main/java/ssgstringManipulation/StirngManipulation.java
+++ src/main/java/ssgstringManipulation/StirngManipulation.java
@@ -6,7 +6,6 @@ public class StirngManipulation {
         String str = "Java kolay";
         System.out.println(""+str.charAt(0)+str.charAt(str.length()-1));
 
-
         StringBuilder strB = new StringBuilder();
         strB.append(str);
         System.out.println(strB.reverse());
