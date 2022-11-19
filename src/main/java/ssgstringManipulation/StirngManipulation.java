166f47c7813a1f3cc13ea6c503de631baedf9918
Mustafa Keser <keserzade@gmail.com>
2022-11-20 00:15:57 +0300

    1

--- src/main/java/ssgstringManipulation/StirngManipulation.java
+++ src/main/java/ssgstringManipulation/StirngManipulation.java
@@ -7,6 +7,12 @@ public class StirngManipulation {
         System.out.println(""+str.charAt(0)+str.charAt(str.length()-1));
 
 
+        StringBuilder strB = new StringBuilder();
+        strB.append(str);
+        System.out.println(strB.reverse());
+
+
+
 
 
 
